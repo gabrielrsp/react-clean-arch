85033df35a0cc7b2897eebafb2502398c9abfce0
unknown <gabriel.pires@a3data.com.br>
2022-07-29 10:00:02 -0300

    move SUT creation to a factory method

--- src/data/useCases/authentication/remote-authentication.spec.ts
+++ src/data/useCases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,27 @@
 import { HttpPostClientSpy } from "../../test/mock-http-client"
 import { RemoteAuthentication } from "./remote-authentication"
 
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makSut = (url:string = 'any_url'): SutTypes => { //factory
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)  // sut: system under test 
+
+  return {
+    sut,
+    httpPostClientSpy
+  }
+} 
+  
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)  // sut: system under test 
 
+    const url = 'other_url'
+    const {sut, httpPostClientSpy} = makSut(url)
     await sut.auth()  // o metodo auth chama o metodo post 
     expect(httpPostClientSpy.url).toBe(url)
   })
