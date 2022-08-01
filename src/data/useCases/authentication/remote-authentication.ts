c6ed9a406c88b8e285470d704e4ec75b21a78dd1
unknown <gabriel.pires@a3data.com.br>
2022-08-01 14:09:31 -0300

    create throw error cases

--- src/data/useCases/authentication/remote-authentication.ts
+++ src/data/useCases/authentication/remote-authentication.ts
@@ -2,6 +2,7 @@ import { HttpPostClient } from "../../protocols/http/http-post-client";
 import { AuthenticationParams } from "../../../domain/useCases/authentication";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
+import { UnexpectedError } from "@/domain/errors/unexpected-error";
 
 //recebe uma url, e um http client como parametros de construtores, e com o metodo auth, faz a chamada da url a partir do http client
 export class RemoteAuthentication {
@@ -17,8 +18,9 @@ export class RemoteAuthentication {
       body: params
     }) 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError
-      default: return Promise.resolve()
+      case HttpStatusCode.ok: break
+      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: throw new UnexpectedError()
     }
   }
 }
