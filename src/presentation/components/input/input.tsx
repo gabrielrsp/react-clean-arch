a092f12c8570825d0890438a3f49c3fb2d16cafd
gabrielrsp <gabrielrsp18@gmail.com>
2022-08-15 20:18:13 -0300

    feat: ensure login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -21,11 +21,11 @@ export const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return  error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'All set!'
   }
 
   return (
