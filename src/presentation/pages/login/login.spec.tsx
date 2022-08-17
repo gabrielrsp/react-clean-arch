8a9373f353c6db1c3ab04254357a13debd15e48a
gabrielrsp <gabrielrsp18@gmail.com>
2022-08-16 21:45:08 -0300

    feat: ensure login present loading spinner on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -17,7 +17,7 @@ const makeSut = (params?: SutParams): SutTypes => {  // FACTORY
   validationStub.errorMessage = params?.validationError
   const sut = render(<Login validation={validationStub} />)
   return {
-    sut
+    sut  
   }
 }
 
@@ -94,6 +94,22 @@ describe('Login component', () => {
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(false)
 
+  })
+
+
+  test('Should show spinner on submit', () => {
+    const { sut } = makeSut()
+
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email()} })
+
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password()} })
+    
+    const submitButton = sut.getByTestId('submit')
+    fireEvent.click(submitButton)
+    const spinner = sut.getByTestId('spinner')
+    expect(spinner).toBeTruthy()
   })
 
 })
